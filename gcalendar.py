f6eeaad0d08d2b8d439eca1a497b9e8ac32a5153
Gavin Etheridge <28293484+gretheridgefh@users.noreply.github.com>
2022-09-13 18:11:39 +0100

    SCOPES = [     'https://www.googleapis.com/auth/calendar.readonly']

--- gcalendar.py
+++ gcalendar.py
@@ -22,6 +22,7 @@ class Gcalendar:
 
     def gcal_connect(self, cal_key):
         creds = None
+        SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
         # The file token.json stores the user's access and refresh tokens, and is
         # created automatically when the authorization flow completes for the first
         # time.
