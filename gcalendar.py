cb62a2aecdadff28f421017b03657ef07eb785d1
Gavin Etheridge <28293484+gretheridgefh@users.noreply.github.com>
2022-09-13 18:04:57 +0100

    x2

--- gcalendar.py
+++ gcalendar.py
@@ -55,45 +55,45 @@ class Gcalendar:
         #    creds.refresh(Request())
         '''
 
-  creds = None
-   # The file token.json stores the user's access and refresh tokens, and is
-   # created automatically when the authorization flow completes for the first
-   # time.
-   if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
 
-    service = build('calendar', 'v3', credentials=creds)
-    print('\n')
-    print('## done service = build :')
-    print('## service :', service)
-    print('\n')
-    # Call the Calendar API
-    now = datetime.datetime.now()  # .isoformat()  # + 'Z'  # 'Z' indicates UTC time
-    today = datetime.datetime(now.year, now.month, now.day)
-    tomorrow = today + datetime.timedelta(days=1)
-    now = now.isoformat()
-    today = today.isoformat() + 'Z'
-    tomorrow = tomorrow.isoformat() + 'Z'
+        service = build('calendar', 'v3', credentials=creds)
+        print('\n')
+        print('## done service = build :')
+        print('## service :', service)
+        print('\n')
+        # Call the Calendar API
+        now = datetime.datetime.now()  # .isoformat()  # + 'Z'  # 'Z' indicates UTC time
+        today = datetime.datetime(now.year, now.month, now.day)
+        tomorrow = today + datetime.timedelta(days=1)
+        now = now.isoformat()
+        today = today.isoformat() + 'Z'
+        tomorrow = tomorrow.isoformat() + 'Z'
 
-    events_result = service.events().list(calendarId=cal_key, timeMin=today,
-                                          maxResults=100, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
+        events_result = service.events().list(calendarId=cal_key, timeMin=today,
+                                            maxResults=100, singleEvents=True,
+                                            orderBy='startTime').execute()
+        events = events_result.get('items', [])
 
-    if not events:
-        return 'No upcoming events found.'
-    return self.set_data(events, cal_key)
+        if not events:
+            return 'No upcoming events found.'
+        return self.set_data(events, cal_key)
 
 
 '''WAS WORKING LOCALLY WITH DESKTOP API KEY
